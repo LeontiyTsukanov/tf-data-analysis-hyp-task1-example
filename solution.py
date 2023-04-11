3ebce131e4ed87090b2157034c17282c4cf6a2a7
Leontiy Tsukanov <122561265+LeontiyTsukanov@users.noreply.github.com>
2023-04-11 22:34:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,6 +11,6 @@ def solution(x_success: int,
     x = x_success/x_cnt
     y = y_success/y_cnt
     flag = False
-    if x/y > 1.04:
+    if x/y > 1.2:
         flag = True
     return flag # Ваш ответ, True или False
